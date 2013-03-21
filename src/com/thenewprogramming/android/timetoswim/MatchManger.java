4c916f8c210ac15e0ad6196b09dd534d290d355b
Reijer the Coder <reijerthecoder@thenewprogramming.com>
2013-03-21 12:52:04 +0100

    added addMatch() in matchmanager, still got to check if thats done right

--- src/com/thenewprogramming/android/timetoswim/MatchManger.java
+++ src/com/thenewprogramming/android/timetoswim/MatchManger.java
@@ -27,7 +27,19 @@ public class MatchManger {
 			Races[i] = BackupOfRaces[i];
 		}
 		Races[oldLengthOfRaces] = new Race(NextMatchId, match, resulttime, type);
+		NextRaceId++;
+	}
+	
+	
+	public void addMatch(int[] date,Race[] races){
+		int oldLengthOfMatches = Matches.length;
+		Match[] BackupOfMatches = Matches.clone();
+		Matches = new Match[oldLengthOfMatches+1];
+
+		for(int i = 0; i < oldLengthOfMatches; i++){
+			Matches[i] = BackupOfMatches[i];
+		}
+		Matches[oldLengthOfMatches] = new Match(NextMatchId);
 		NextMatchId++;
 	}
-
 }
