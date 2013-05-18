5a6436c46889989e5d22370228e29cf3dec7dbfb
Reijer The Coder (Thuis PC) <reijerthecoder@thenewprogramming.com>
2013-05-18 13:10:44 +0200

    installed Android Studio!

--- src/com/thenewprogramming/android/timetoswim/AddRaceFragment.java
+++ src/com/thenewprogramming/android/timetoswim/AddRaceFragment.java
@@ -10,6 +10,7 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class AddRaceFragment extends DialogFragment implements OnItemSelectedListener{
 
@@ -40,9 +41,12 @@ public class AddRaceFragment extends DialogFragment implements OnItemSelectedLis
 	}
 
 	@Override
-	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+	public void onItemSelected(AdapterView<?> parent, View view,  int pos, long id) {
 		Spinner DistanceSpinner = (Spinner) getActivity().findViewById(R.id.AddRaceDialog_StrokeSelector);
-		
+		String SelectedtextView = (String) parent.getItemAtPosition(pos);
+		if(SelectedtextView.getText().equals()){
+			
+		}
 		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.Distances, android.R.layout.simple_spinner_item);
 		
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
